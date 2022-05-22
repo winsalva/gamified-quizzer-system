f621ad1c0ba15948dc0d44bcef8f9db6eb26e2c4
* <winsalva396@gmail.com>
2022-05-22 09:35:53 +0800

    Added fallback page

--- lib/app_web/controllers/page_controller.ex
+++ lib/app_web/controllers/page_controller.ex
@@ -1,5 +1,9 @@
 defmodule AppWeb.PageController do
   use AppWeb, :controller
+
+  def fallback(conn, _params) do
+    render(conn, "fallback.html")
+  end
   
   def index(conn, _params) do
     render(conn, :index)
